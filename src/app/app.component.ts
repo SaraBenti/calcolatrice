175b3f1d0d188b06ce2e4a7b4f17c50e6f148eee
Sara Benti <sara.bentivegni@enaipriminifad.org>
2023-05-10 15:17:34 +0200

    ex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,77 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'calcolatrice';
+  testoDisplay:string="";
+  
+  operando1:number=0;
+  operando2:number=0;
+  operatore:string="";
+setOperatore=false;
+daCalcolare="";
+result=false;
+
+
+  pressButton(but: string) {
+    if (but === '/' || but === '*' || but === '-' || but === '+') {
+      const lastBut = this.testoDisplay[this.testoDisplay.length - 1];
+      console.log(lastBut);
+      if (lastBut === '/' || lastBut === '*' || lastBut === '-' || lastBut === '+')  {
+        this.setOperatore = true;
+      }
+      if ((this.setOperatore) || (this.testoDisplay === '')) {
+        return;
+      }
+      this.operando1 = parseFloat(this.testoDisplay);
+      this.operatore = but;
+      this.setOperatore = true;
+   }
+   if (this.testoDisplay.length === 10) {
+     return;
+   }
+   this.testoDisplay += but;
+}
+getResult() {
+  this.daCalcolare = this.testoDisplay;
+  this.operando2 = parseFloat(this.testoDisplay.split(this.operatore)[1]);
+  if (this.operatore === '/') {
+  
+    this.testoDisplay = (this.operando1 / this.operando2).toString();
+    if (this.testoDisplay.length > 9) {
+      this.testoDisplay = this.testoDisplay.substring(0, 9);
+    }
+  } else if (this.operatore === '*') {
+   
+    this.testoDisplay = (this.operando1 * this.operando2).toString();
+
+    if (this.testoDisplay.length > 9) {
+      this.testoDisplay = 'ERROR';
+    
+    }
+  } else if (this.operatore === '-') {
+   
+    this.testoDisplay = (this.operando1 - this.operando2).toString();
+  
+  } else if (this.operatore === '+') {
+   
+    this.testoDisplay = (this.operando1 + this.operando2).toString();
+  
+    if (this.testoDisplay.length > 9) {
+      this.testoDisplay = 'ERROR';
+    
+    }
+  } else {
+    this.testoDisplay = 'ERROR: Invalid Operation';
+  }
+  this.result = true;
+}
+ 
+getReset(){
+  this.testoDisplay = '';
+
+  this.setOperatore = false;
+}
 }
